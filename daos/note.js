c109b57843c59ba1123b2dfc7c2045d1d6eba904
Yavor Papazov <y_papaz@yahoo.com>
2023-05-05 13:02:26 -0500

    commit

--- daos/note.js
+++ daos/note.js
@@ -4,49 +4,15 @@ const Note = require('../models/note');
 
 module.exports = {};
 
-// module.exports.getSearch = async (page, perPage, query) => { 
-//   if (query) {
-//     return await Book.find({ 
-//       $text: { $search: query } },
-//       { score: { $meta: 'textScore'}}
-//     ).sort({ score: { $meta: 'textScore'}}).limit(perPage).skip(perPage*page).lean();
-//   }
-//   return await Book.find().limit(perPage).skip(perPage*page).lean();
-// }
-
-// module.exports.getStats = (authorInfo) => {
-//   if (authorInfo) {
-//     return Book.aggregate([
-//       { $group: { _id: '$authorId', averagePageCount: { $avg: '$pageCount' }, numBooks: { $sum: 1 }, titles: { $push: '$title' } } },
-//       { $project: { _id: 0, authorId: '$_id', averagePageCount: 1, numBooks: 1, titles: 1 } },
-//       { $lookup: {
-//         from: 'authors',
-//         localField: 'authorId',
-//         foreignField: '_id',
-//         as: 'author'
-//       }},
-//       { $unwind: '$author' }
-//     ])
-//   }
-//   return Book.aggregate([
-//     { $group: { _id: '$authorId', averagePageCount: { $avg: '$pageCount' }, numBooks: { $sum: 1 }, titles: { $push: '$title' } } },
-//     { $project: { _id: 0, authorId: '$_id', averagePageCount: 1, numBooks: 1, titles: 1 } }
-//   ])
-// }
-
-// module.exports.getById = (bookId) => {
-//   if (!mongoose.Types.ObjectId.isValid(bookId)) {
-//     return null;
-//   }
-//   return Book.findOne({ _id: bookId }).lean();
-// }
-
 module.exports.createNote = async (note) => {
     const created = await Note.create(note);
     return created;
 }
 
 module.exports.getNote = async (userId, noteId) => {
+    if (!mongoose.Types.ObjectId.isValid(noteId)) {
+        return null;
+    }
     return await Note.find({ _id: noteId, userId }).lean();
 }
 
--- routes/notes.js
+++ routes/notes.js
@@ -11,7 +11,7 @@ router.use(async function (req, res, next) {
         req.userId = userId;
     }
     next();
-})
+});
 
 router.post("/", async (req, res, next) => {
     const note = {
@@ -24,36 +24,18 @@ router.post("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
     const note = await noteDAO.getNote(req.userId, req.params.id);
-    res.json(note[0]);
+    if (!note) {
+        res.sendStatus(400);
+    } else if (note.length === 0) {
+        res.sendStatus(404);
+    } else {
+        res.json(note[0]);
+    }
 });
 
 router.get("/", async (req, res, next) => {
     const notes = await noteDAO.getUserNotes(req.userId);
     res.json(notes);
 });
-
-// router.get("/search", async (req, res, next) => {
-//   let { page, perPage, query } = req.query;
-//   page = page ? Number(page) : 0;
-//   perPage = perPage ? Number(perPage) : 10;
-//   const books = await bookDAO.getSearch(page, perPage, query);
-//   res.json(books);
-// });
-
-// router.get("/authors/stats", async (req, res, next) => {
-//   const { authorInfo } = req.query;
-//   const stats = await bookDAO.getStats(authorInfo);
-//   res.json(stats);
-// });
-
-// // Read - single book
-// router.get("/:id", async (req, res, next) => {
-//   const book = await bookDAO.getById(req.params.id);
-//   if (book) {
-//     res.json(book);
-//   } else {
-//     res.sendStatus(404);
-//   }
-// });
 
 module.exports = router;
