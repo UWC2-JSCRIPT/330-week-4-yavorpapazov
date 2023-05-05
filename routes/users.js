99e81e847d1852de59f24dc7871f8df46ef495ab
Yavor Papazov <y_papaz@yahoo.com>
2023-05-05 14:31:10 -0500

    commit

--- routes/users.js
+++ routes/users.js
@@ -46,19 +46,25 @@ router.post("/", async (req, res, next) => {
             if (e instanceof userDAO.BadDataError) {
                 res.status(400).send(e.message);
             } else {
-                res.status(500).send(e.message);
+                res.status(401).send(e.message);
             }
         }
     }
 });
 
 router.use(async function (req, res, next) {
-    const tokenString = req.headers.authorization.slice(7);
-    const userId = await userDAO.getUserIdFromToken(tokenString);
-    if (userId) {
-        req.userId = userId;
+    if (!req.headers.authorization) {
+        res.sendStatus(401);
+    } else {
+        const tokenString = req.headers.authorization.slice(7);
+        const userId = await userDAO.getUserIdFromToken(tokenString);
+        if (userId) {
+            req.userId = userId;
+            next();
+        } else {
+            res.sendStatus(401);
+        }
     }
-    next();
 });
 
 router.post("/password", async (req, res, next) => {
